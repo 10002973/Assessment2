758e55c3ccffe47508372432e64cec9d9fd28022
Heather <Heather@DESKTOP-PJVST6L>
2018-04-27 16:55:05 +0100

    ClockTest JUnit completed

---
+++ test/clock/ClockTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clock;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Heather
+ */
+public class ClockTest {
+    
+    public ClockTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Clock.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        Clock.main(args);
+    }
+    
+}
