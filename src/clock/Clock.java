a15b150d7009d7613ea2ba097a02e73e5c4378ef
Heather <Heather@DESKTOP-PJVST6L>
2018-04-28 15:06:15 +0100

    Added comments and JavaDoc

--- src/clock/Clock.java
+++ src/clock/Clock.java
@@ -5,8 +5,22 @@ import java.text.ParseException;
 import queuemanager.QueueOverflowException;
 import queuemanager.QueueUnderflowException;
 
+/**
+ *
+ * @author Heather Taylor-Stanley 10002973
+ * 
+ * 
+ */
 public class Clock {
     
+    /**
+     *
+     * @param args
+     * @throws QueueOverflowException
+     * @throws QueueUnderflowException
+     * @throws IOException
+     * @throws ParseException
+     */
     public static void main(String[] args) throws QueueOverflowException, QueueUnderflowException, IOException, ParseException {
         Model model = new Model();
         View view = new View(model);
